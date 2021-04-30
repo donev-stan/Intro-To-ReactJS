b255c445382f7d1dc2fa94d82c645dbf7e98f8bb
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-04-30 20:18:03 +0300

    redux setup

--- redux-intro/src/Counter.jsx
+++ redux-intro/src/Counter.jsx
@@ -1,14 +1,14 @@
 import { React, useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
 
     const increment = () => {
-        setCount(count + 1);
+        // setCount(count + 1);
     };
 
     const decrement = () => {
-        setCount(count - 1);
+        // setCount(count - 1);
     };
 
     return (
