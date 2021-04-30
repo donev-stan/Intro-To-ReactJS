2a7af521e90b74b4aca72fa7de595d75f317cbbb
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-04-30 19:53:12 +0300

    inital app state - boilerplate

---
+++ redux-intro/src/Counter.jsx
@@ -0,0 +1,27 @@
+import { React, useState } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <div className="container my-4 border">
+            <div className="row">
+                <div className="col">
+                    <h3>{`Count: ${count}`}</h3>
+                    <button className="btn btn-success m-2" onClick={increment}> Increment </button>
+                    <button className="btn btn-danger m-2" onClick={decrement}> Decrement </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Counter;
