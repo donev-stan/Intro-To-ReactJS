53b2917413418a2ff6d98fcc5856b36d0c0a21c8
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2021-06-12 21:09:54 +0300

    .

--- uni-project-user-tasks/src/components/users/Users.jsx
+++ uni-project-user-tasks/src/components/users/Users.jsx
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { getAllUsers } from '../../core/services/UsersService';
-import User from './User';
+import { User } from './User';
 
 export const Users = () => {
 
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        getAllUsers().then(response => {setUsers(response.data)});
+        // getAllUsers().then(response => {setUsers(response.data)});
+        getAllUsers().then(response => {console.log(response.data)});
     }, [])
 
 
     return (
         <div className="container-fluid">
-            { users.map(user => <User user={user} />) }
+            { users.map(user => <User key={user.id} user={user} />) }
         </div>
     )
 }
